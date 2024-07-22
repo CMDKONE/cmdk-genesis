74fd6b835dc4038c9dde71cf97f079536c0a4c81
Nic Smith <thesonicsmith@gmail.com>
2024-07-23 11:28:50 +1200

    feat: add 404 faucet to TestnetFaucet

--- src/TestnetFaucet.sol
+++ src/TestnetFaucet.sol
@@ -2,12 +2,14 @@
 pragma solidity 0.8.26;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC404} from "erc404/interfaces/IERC404.sol";
 
 contract TestnetFaucet {
-    using SafeERC20 for IERC20;
+    function getERC20Tokens(address tokenAddress, uint256 amount) external {
+        IERC20(tokenAddress).transfer(msg.sender, amount);
+    }
 
-    function getTokens(address tokenAddress, uint256 amount) external {
-        IERC20(tokenAddress).safeTransfer(msg.sender, amount);
+    function getERC404Tokens(address tokenAddress, uint256 amount) external {
+        IERC404(tokenAddress).transfer(msg.sender, amount);
     }
 }
