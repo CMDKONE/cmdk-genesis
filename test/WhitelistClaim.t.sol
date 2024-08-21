7cfa4dcd281f7a2a31944cc396e51746e981e671
sonicsmith <thesonicsmith@gmail.com>
2024-08-21 15:53:47 +1200

    Add merkle claim tests

--- test/WhitelistClaim.t.sol
+++ test/WhitelistClaim.t.sol
@@ -5,22 +5,42 @@ import {Test, console} from "forge-std/Test.sol";
 import {WhitelistClaim} from "../src/WhitelistClaim.sol";
 import {CMDKGenesisKit} from "../src/CMDKGenesisKit.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Merkle} from "murky/Merkle.sol";
 
 contract WhitelistClaimTest is Test {
-    CMDKGenesisKit public cmdkGenesisKit;
-    WhitelistClaim public whitelistClaim;
+    CMDKGenesisKit cmdkGenesisKit;
+    WhitelistClaim whitelistClaim;
+    Merkle merkleTree;
+    bytes32[] aliceProof;
+    bytes32[] bobProof;
 
     address owner = address(1);
     address stranger = address(2);
     address alice = address(3);
     address bob = address(4);
+    uint256 aliceAmount = 123;
+    uint256 bobAmount = 456;
+
+    error AlreadyClaimed();
 
     function setUp() public {
         cmdkGenesisKit = new CMDKGenesisKit(owner);
         whitelistClaim = new WhitelistClaim(owner, address(cmdkGenesisKit));
+        vm.prank(owner);
+        cmdkGenesisKit.setERC721TransferExempt(address(whitelistClaim), true);
+        vm.prank(owner);
+        cmdkGenesisKit.transfer(address(whitelistClaim), 100 ether);
+        // Merkle tree
+        merkleTree = new Merkle();
+        bytes32[] memory data = new bytes32[](2);
+        data[0] = keccak256(abi.encodePacked(alice, aliceAmount));
+        data[1] = keccak256(abi.encodePacked(bob, bobAmount));
+        bytes32 root = merkleTree.getRoot(data);
+        vm.prank(owner);
+        whitelistClaim.setMerkleRoot(root);
+        aliceProof = merkleTree.getProof(data, 0);
+        bobProof = merkleTree.getProof(data, 0);
     }
-
-    function test_setup() public view {}
 
     function test_setMerkleRoot_onlyOwner_revert() public {
         vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, stranger));
@@ -35,6 +55,17 @@ contract WhitelistClaimTest is Test {
     }
 
     function test_claim() public {
-        //
+        vm.prank(alice);
+        whitelistClaim.claim(aliceAmount, aliceProof);
+        assertEq(cmdkGenesisKit.balanceOf(alice), aliceAmount);
+    }
+
+    function test_claim_claimTwice_revert() public {
+        vm.prank(alice);
+        whitelistClaim.claim(aliceAmount, aliceProof);
+        vm.expectRevert(AlreadyClaimed.selector);
+        vm.prank(alice);
+        whitelistClaim.claim(aliceAmount, aliceProof);
+        assertEq(cmdkGenesisKit.balanceOf(alice), aliceAmount);
     }
 }
